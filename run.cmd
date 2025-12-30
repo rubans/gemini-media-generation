287bbc9a2e9e903369bdfe010673c9fe33a93dd7
rubans <rubansiva@hotmail.com>
2025-12-30 10:55:31 +0000

    update path issue with cmd script

--- run.cmd
+++ run.cmd
@@ -1,2 +1,11 @@
 @echo off
-uv run --with fastmcp --with-requirements mcp\requirements.txt fastmcp run mcp\servers\gemini_media_gen.py
+setlocal
+
+:: Store the script's directory in a variable.
+:: The trailing backslash is already included.
+set "SCRIPT_DIR=%~dp0"
+
+echo The script's root directory is: %SCRIPT_DIR%
+uv run --with fastmcp --with-requirements "%SCRIPT_DIR%\mcp\requirements.txt" fastmcp run "%SCRIPT_DIR%\mcp\servers\gemini_media_gen.py"
+
+endlocal
